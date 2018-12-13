981b9d96b3999b7e3754440cf76735e5a4646051
jensbackvall <cool42dude>
2018-12-13 13:28:59 +0100

    add fields for editing

--- public/media_coverage/media_coverage.js
+++ public/media_coverage/media_coverage.js
@@ -26,12 +26,39 @@ $.ajax({
         const htmlString = "<li><div id='" + mediaId + "'><p><h3><span class='dateFormat'>" + publishDate + ": </span><a href='" + mediaLink + "'>" + mediaTitle + "</a></h3></p><p>Source: " + mediaSource + "</p></div></li>";
 
         $("#" + jsonList[i]['country'].toLowerCase()).append(htmlString); 
-        
-        // check hvis admin er logget ind
-        // hvis ja, indsæt redigeringsknap samt redigeringsfelter for hvert element i dette li tag
 
+        // check hvis admin er logget ind
+        if (window.localStorage.getItem('user') === 'admin') {
+        // hvis ja, indsæt redigeringsknap samt redigeringsfelter for hvert element i dette li tag
+            $("#" + mediaId).append("<button type='submit' id='editButton" + mediaId + "'>Edit Item Above</button>");
+            $("#" + mediaId).append(`<form id="editform${mediaId}">
+            <input type="text" class="publish_date" name="dateField${mediaId}" id="dateField${mediaId}" placeholder="${publishDate}">
+            <br><br>
+            <input type="text" class="media_title" name="titleField${mediaId}" id="titleField${mediaId}" placeholder="${mediaTitle}">
+            <br><br>
+            <input type="text" class="media_link" name="linkField${mediaId}" id="linkField${mediaId}" placeholder="${mediaLink}">
+            <br><br>
+            <input type="text" class="media_source" name="sourceField${mediaId}" id="sourceField${mediaId}" placeholder="${mediaSource}">
+            <br><br>
+            <button type="submit" id="sendChangesButton${mediaId}">
+                Save Changes
+            </button>
+            <br>
+            </form>`);
+            $(`#editform${mediaId}`).css("display", "none");
+        }
+
+        $("#editButton" + mediaId).click((event) => {
+            console.log("editButton " + mediaId + " pressed");
+            $(`#editform${mediaId}`).css("display", "");      
+        });
+        $("#sendChangesButton" + mediaId).click((event) => {
+            console.log("sendChangesButton " + mediaId + " pressed");
+            $(`#editform${mediaId}`).css("display", "none");      
+        });
     }
+    
 });
 
-//date title source link
 
+// add boolean isCollapsed which controls display:none on form
